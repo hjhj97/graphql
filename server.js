1f40db581e4c9b9f92ad3f6282575fb971f73ad9
JuHeonHa <hjhjaueon97@naver.com>
2023-01-01 18:00:02 +0900

    add query,mutation

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import { ApolloServer, gql } from "apollo-server";
 const typeDefs = gql`
-  type Query {
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
     text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
+  }
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    deleteTweet(id: ID): Boolean
   }
 `;
 
